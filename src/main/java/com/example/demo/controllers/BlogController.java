f7d25153de0ec7f56af2ecab9d935c2bcdce9c7a
rauf tababilov <tababilovrauf@gmail.com>
2020-05-19 19:52:10 +0500

    create blog-details

--- src/main/java/com/example/demo/controllers/BlogController.java
+++ src/main/java/com/example/demo/controllers/BlogController.java
@@ -2,13 +2,14 @@ package com.example.demo.controllers;
 
 import com.example.demo.models.Post;
 import com.example.demo.repo.PostRepository;
+import javafx.geometry.Pos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -33,5 +34,18 @@ public class BlogController {
         Post post = new Post(title, anons, full_text);
         postRepository.save(post);
         return "redirect:/blog";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String blogDetails(@PathVariable(value = "id") long id, Model model) {
+        if (!postRepository.existsById(id)) {
+            return "redirect:/blog";
+        }
+
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "blog-details";
     }
 }
